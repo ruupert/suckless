770f0405c9215495ed1a13d0449a171fac805bb9
Raul Becker <raul.becker@iki.fi>
2024-04-29 02:11:00 +0300

    err

--- setup.sh
+++ setup.sh
@@ -3,5 +3,5 @@ VENVDIR=.venv
 if [ -d "$VENVDIR" ]; then
     python3 -m venv $VENVDIR
 fi
-python3 $VENVDIR/bin/activate
+source $VENVDIR/bin/activate
 pip3 install -r requirements.txt
--- test.sh
+++ test.sh
@@ -3,7 +3,7 @@ VENVDIR=.venv
 if [ -d "$VENVDIR" ]; then
     python3 -m venv $VENVDIR
 fi
-python3 $VENVDIR/bin/activate
+source $VENVDIR/bin/activate
 pip3 install -r requirements.txt
 
 vagrant up --no-parallel
