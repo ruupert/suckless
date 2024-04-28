58b7b1bf2ae6404616329ee3b7edc5007e8f64c7
Raul Becker <raul.becker@iki.fi>
2024-04-28 16:46:02 +0300

    test each individually, fixed install deps, testing --no-parallel quite slow now

---
+++ test.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+VENVDIR=.venv
+if [ -d "$VENVDIR" ]; then
+    python3 -m venv $VENVDIR
+fi
+python3 $VENVDIR/bin/activate
+pip3 install -r requirements.txt
+
+vagrant up --no-parallel
+vagrant destroy -f
